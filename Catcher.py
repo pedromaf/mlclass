750379b556dd3ad3f20e3c0f999dee4ae6ad6017
Pedro Marinho <pmaf@ic.ufal.br>
2019-08-08 17:07:34 -0300

    Update Catcher.py

--- Catcher.py
+++ Catcher.py
@@ -16,20 +16,28 @@ class RandomAgent:
 				for action in range(0, 3):
 					self.q_table[player_x][fruit_x][action] = random.randrange(0, 100)
 
-	def maxQAction(state):
-		player_x = state.get("player_x")
-		fruit_x = state.get("fruit_x")
-		return max(self.q_table[player_x][fruit_x][0], self.q_table[player_x][fruit_x][1], self.q_table[player_x][fruit_x][2])
-
 	def pickAction(self, state):
 		if random.uniform(0, 1)  <= exploration_rate:
 			#Exploration
 			return random.choice(self.actions)
 		else:
 			#Exploitation
-			return maxQAction(state)
+			return self.maxQAction(state)
 
-def reward(state0, state1, fruit_reward):
+	def maxQAction(self, state):
+		player_x = state.get("player_x")
+		fruit_x = state.get("fruit_x")
+		return max(self.q_table[player_x][fruit_x][0], self.q_table[player_x][fruit_x][1], self.q_table[player_x][fruit_x][2])
+
+def actionIndex(action):
+    if action == 97:
+        return 0
+    elif action == 100:
+        return 1
+    else:
+        return 2        
+
+def Reward(state0, state1, fruit_reward):
 	state0_player_x = state0.get("player_x")
 	state0_fruit_x = state0.get("fruit_x")
 	state1_player_x = state1.get("player_x")
@@ -38,7 +46,7 @@ def reward(state0, state1, fruit_reward):
 	state0_goal_distance = pow(state0_fruit_x - state0_player_x, 2)
 	state1_goal_distance = pow(state1_fruit_x - state1_player_x, 2)
 
-	goal_progress = state0_goal_distance/state1_goal_distance
+	goal_progress = int((state0_goal_distance/state1_goal_distance))
 
 	if goal_progress < 1:
 		return ((-goal_progress) + (fruit_reward*100))
@@ -62,7 +70,6 @@ p = PLE(game, fps=30, display_screen=True, force_fps=False)
 p.init()
 
 agent = RandomAgent(p.getActionSet())
-print(agent.q_table)
 nb_frames = 1000
 reward = 0.0
 
@@ -74,8 +81,9 @@ for f in range(nb_frames):
 		p.reset_game()
 
 	state0 = game.getGameState()
-	action = agent.pickAction(state0)
-	state1 = game.getGameState()
-	reward = reward(state0, state1, p.act(action))
-	current_Q = RandomAgent.q_table[state0.get("player_x")][state0.get("fruit_x")][action]
-	RandomAgent.q_table[state0.get("player_x")][state0.get("fruit_x")][action] = current_Q + alpha*(reward + gamma*(RandomAgent.maxQAction(state1)) - current_Q)
+    action = agent.pickAction(state0)
+    state1 = game.getGameState()
+    reward = Reward(state0, state1, p.act(action))
+    action_index = actionIndex(action)
+    current_Q = int(agent.q_table[state0.get("player_x")][state0.get("fruit_x")][action_index])
+    agent.q_table[state0.get("player_x")][state0.get("fruit_x")][action] = current_Q + alpha*(reward + gamma*(agent.maxQAction(state1)) - current_Q)
